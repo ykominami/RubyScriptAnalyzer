451cd3cd4dc064af3cf51dc4491af6111dbf0bdf
yasuo kominami <ykominami@gmail.com>
2021-10-20 11:49:54 +0900

    use class instance variable and class methods

--- lib/RubyAnalyzer/reflection/analyzer/analyzerfiles.rb
+++ lib/RubyAnalyzer/reflection/analyzer/analyzerfiles.rb
@@ -7,6 +7,21 @@ module RubyAnalyzer
 
       include Utilmod2
 
+			@names = [
+				:target_class_list,
+				:target_module_list,
+				:target_instance_list,
+				:exclude_class_list,
+				:exclude_module_list,
+				:exclude_instance_list,
+				:exclude_const_list,
+			]
+			@analyzerfile_ = Struct.new("AnalyzerFilesx", *@names.map{|x| x})
+
+			def self.get_analyzerfile
+				@analyzerfile_
+			end
+
 			def initialize(target_classname_fname, target_modulename_fname, target_instancename_fname,
 										exclude_classname_fname, exclude_modulename_fname, exclude_instancename_fname,
 										exclude_constname_fname)
@@ -19,8 +34,7 @@ module RubyAnalyzer
 					[:exclude_instance_list, exclude_instancename_fname],
 					[:exclude_const_list, exclude_constname_fname]
 				]
-				analyzerfile_ = Struct.new("AnalyzerFilesx", *@names.map{|x| x[0]})
-				@analyzerfile = analyzerfile_.new(*@names.map{|x| x[1]})
+				@analyzerfile = self.class.get_analyzerfile.new(*@names.map{|x| x[1]})
 				@analyzerfile.members.map{ |list_key|
 					list = File.readlines(@analyzerfile[list_key])
 					list2 = list.map { |l| l.split("#").first }
