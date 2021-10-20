98da3c0e3d434a67b3dff9efa12de3db6b83386d
yasuo kominami <ykominami@gmail.com>
2021-10-20 11:41:17 +0900

    use struct in anlyzerfiles and analyerresult

--- lib/RubyAnalyzer/reflection/analyzer/analyzerresult.rb
+++ lib/RubyAnalyzer/reflection/analyzer/analyzerresult.rb
@@ -4,82 +4,27 @@ require "RubyAnalyzer/utilmod2"
 
 module RubyAnalyzer
   class Analyzer
+    @names = [
+      [:class_list, Setx.new],
+      [:module_list, Setx.new],
+      [:instance_list, Setx.new],
+      [:exclude_class_list, Setx.new],
+      [:exclude_module_list, Setx.new],
+      [:exclude_instance_list, Setx.new],
+      [:exclude_const_list, Setx.new],
+      [:unknown_class_list, Setx.new],
+      [:unknown_module_list, Setx.new],
+      [:unknown_instance_list, Setx.new],
+      [:not_respond_to_list, Setx.new]
+    ]
+    AnalyzerResult = Struct.new(*@names.map{|n| n[0]})
+
     class AnalyzerResult
-      attr_accessor :class_list, :module_list, :instance_list, :exclude_class_list, 
-                    :exclude_module_list, :exclude_instance_list, :exclude_const_list,
-                    :unknown_class_list, :unknown_module_list, :unknown_instance_list,
-                    :not_respond_to_list
       include Utilmod2
+    end
 
-      def initialize
-        @hs = {}
-        @names = %W!
-          class_list
-          module_list
-          instance_list
-          exclude_class_list
-          exclude_module_list
-          exclude_instance_list
-          exclude_const_list
-          unknown_class_list
-          unknown_module_list
-          unknown_instance_list
-          not_r@espond_to_list
-        !
-        @names.map{ |name| @hs[name] = Setx.new}
-      end
-
-      def class_list
-        @hs["class_list"]
-      end
-
-      def module_list
-        @hs["module_list"]
-      end
-
-      def instance_list
-        @hs["instance_list"]
-      end
-
-      def exclude_class_list
-        @hs["exclude_class_list"]
-      end
-
-      def exclude_module_list
-        @hs["exclude_module_list"]
-      end
-
-      def exclude_instance_list
-        @hs["exclude_instance_list"]
-      end
-
-      def exclude_const_list
-        @hs["exclude_const_list"]
-      end
-
-      def unknown_class_list
-        @hs["unknown_class_list"]
-      end
-
-      def unknown_module_list
-        @hs["unknown_module_list"]
-      end
-
-      def unknown_instance_list
-        @hs["unknown_instance_list"]
-      end
-
-      def not_respond_to_list
-        @hs["not_respond_to_list"]
-      end
-
-      def sort_lists_x!
-        sort_lists!(@hs)
-      end
-
-      def print_lists_x
-        print_lists(@hs)
-      end
+    def self.create_AnalyzerResult
+      AnalyzerResult.new(*@names.map{|n| n[1]}) 
     end
 	end
 end
